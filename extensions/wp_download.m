2651ab7bbdc153d4d4a8d70a9080313c5256e7b6
John Kitchin <jkitchin@cmu.edu>
2012-05-08 13:12:15 -0400

    changed relative path of file to be relative to mfile. This changed because in blogpost I don't change into the html folder anymore.

--- extensions/wp_download.m
+++ extensions/wp_download.m
@@ -1,7 +1,5 @@
 function html = wp_download(datastring)
 % upload file in datastring, and return url to the link
-% the datastring is a filename,relative to one directory up because we
-% are in ../html
-fname = fullfile('../', datastring); %assume fname is relative to one directory up
-url = newMediaObject(fname);
+% the datastring is a filename,relative to the mfile
+url = newMediaObject(datastring);
 html = sprintf('<a href=%s> download %s </a>',url,datastring);

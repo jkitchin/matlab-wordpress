e53510931a09a8adbf5c28ceb0013670aae49329
John Kitchin <jkitchin@cmu.edu>
2012-05-08 13:56:50 -0400

    fixed bug in making a line break after the tooltip.

--- extensions/wp_tooltip.m
+++ extensions/wp_tooltip.m
@@ -14,32 +14,9 @@ end
 tooltip_text = tok{1}{1};
 text_with_tooltip = tok{1}{2};
 
-html = {...
-    '<head>'
-    	'<script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>'
-'<style>'
-'.tooltip {'
-	'display:none;'
-	'background:transparent url(/tools/img/tooltip/black_arrow.png);'
-	'font-size:12px;'
-	'height:70px;'
-	'width:160px;'
-	'padding:25px;'
-	'color:#fff;'	
-'}'
-'#$tooltip_id$ a {'
-	'border:0;'
-	'cursor:pointer;'
-	'margin:0 8px;'
-        'color:blue'
-'}'
-'</style>'
-'</head>'
-'<div id="$tooltip_id$"><a title="$tooltip_text$"/>$$</a></div>'
-'<script>$("#demo").tooltip();</script>'
-};
+html = '<a TITLE="$tooltip_text$" STYLE="COLOR:blue">$$</a>';
 
- % three things to replace:
+% three things to replace:
 % 1. the id $tooltip_id$. this will be a UUID for uniqueness
 % 2. the tooltip text $tooltip_text$
 % 3. the text that has the tooltip on it $$
@@ -48,6 +25,6 @@ html = strrep(html,'$tooltip_id$',UUID);
 html = strrep(html,'$tooltip_text$', tooltip_text);
 html = strrep(html,'$$', text_with_tooltip);
 
-html = strcat(html{:});
+sprintf('"%s"',text_with_tooltip)
  
 end
